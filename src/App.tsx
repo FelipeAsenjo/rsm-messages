4c087acda390b1db1824ccda0f7356b6fd43846b
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-06-09 04:33:07 -0400

    Create header

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,17 @@ function App() {
   return (
     <>
       <div className="flex flex-col justify-center h-dvh">
-        <header className="bg-primary p-4">
-          hola mundo
+        <header className="flex justify-center bg-primary p-2">
+          <div className="flex flex-row gap-2 w-fit bg-white text-text text-center rounded-full p-2">
+            <div className="self-center w-4 h-4 rounded-full bg-secondary"></div>
+            <h1>RSM Chat</h1>
+          </div>
         </header>
-        <main className="flex flex-col h-full w-full max-w-[768px] p-4">
+        <main className="flex flex-col h-full w-full max-w-[768px] p-4 text-text">
 
-        <section className="flex-1">
-          adios mundo
-        </section>
+          <section className="flex-1">
+            adios mundo
+          </section>
 
           <MessagesForm />
         </main>
