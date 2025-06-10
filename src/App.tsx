41b3a07487e8071076c833a956ce7e73535fb219
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-06-10 03:28:13 -0400

    Display messages bottom to top

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import { useChatStore } from "./store/chatStore";
 import MessagesForm from "./components/MessagesForm";
 import Header from "./components/Header";
 
+import IndexedDB from './services/indexedDB';
+const idxDB = new IndexedDB()
+
 const chatService = new RsmChatService()
+const commonMsgStyles = 'flex p-2 w-fit rounded-md text-black'
 
 function App() {
-  const { messages } = useChatStore(state => state)
+  const { messages, setMessages } = useChatStore(state => state)
 
   useEffect(() => {
     const fetchHealth = async () => {
@@ -16,19 +20,32 @@ function App() {
       console.log('data', data)
     }
 
+    const persistanceMessages = async () => {
+      const storedMessages = await idxDB.getAllMessages()
+
+      setMessages(storedMessages)
+    }
+
+    persistanceMessages()
     fetchHealth()
   }, [])
 
   return (
     <>
-      <div className="flex flex-col justify-center h-dvh">
+      <div className="flex flex-col justify-center h-dvh relative">
         <Header />
         <main className="flex flex-col self-center h-full w-full max-w-[768px] p-4 text-text">
 
-          <section className="flex-1">
+          <section className="flex-1 flex flex-col-reverse">
             { messages.map(msg => (
-              <div key={ msg.id }>
-                <p>{ msg.message }</p>
+              <div key={ msg.id } className='flex flex-col-reverse my-1 overflow-y-auto'>
+                <p 
+                  className={msg.sender !== 'Felipe' ? 
+                    `${commonMsgStyles} bg-primary-light` : 
+                    `${commonMsgStyles} self-end bg-secondary-light`
+                }>
+                  { msg.message }
+                </p>
               </div>
             ))}
           </section>
--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -12,7 +12,7 @@ interface ChatStore {
 
 export const useChatStore = create<ChatStore>((set) => ({
     messages: [],
-    addMessage: (message) => set((state) => ({ messages: [...state.messages, message] })),
+    addMessage: (message) => set((state) => ({ messages: [message, ...state.messages] })),
     setMessages: (bulkMessages: IPostMessages[]) => set(() => ({ messages: bulkMessages })),
     updateMessageStatus: (id: number, status: IPostMessages['status']) => set(state => ({
         messages: state.messages.map(message => message.id === id ? { ...message, status } : message)
