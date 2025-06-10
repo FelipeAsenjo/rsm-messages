a5a7809a4337ba60b54428573c6241927de1ebe0
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-06-10 15:59:19 -0400

    Retry POST error message

--- src/components/MessagesDisplay.tsx
+++ src/components/MessagesDisplay.tsx
@@ -1,16 +1,22 @@
 import { MdOutlineSettingsBackupRestore } from "react-icons/md";
+import RsmChat from "../services/rsmChatService";
 
 import { useChatStore } from "../store/chatStore";
 import { useUserStore } from "../store/userStore";
+import type { IPostMessages } from "../interfaces/Chat";
 
 const commonMsgStyles = 'flex p-2 w-fit rounded-md text-black'
+
+const rsmChat = new RsmChat()
 
 export default function MessagesDisplay() {
   const { messages } = useChatStore(state => state)
   const { username } = useUserStore(state => state)
 
-  const handleClick = (id: number | undefined) => {
-    console.log('id', id)
+  const handleClick = async (msg: IPostMessages) => {
+    const res = await rsmChat.postMessages(msg)
+
+    console.log('res', res)
   }
 
   console.log('messages', messages)
@@ -19,7 +25,7 @@ export default function MessagesDisplay() {
         { messages.map(msg => (
           <div key={ msg.id } className='flex flex-col-reverse my-1 overflow-y-auto'>
             { msg.status === 'error' && 
-              <button className="self-end" onClick={() => handleClick(msg.id)}>
+              <button className="self-end" onClick={() => handleClick(msg)}>
                 <MdOutlineSettingsBackupRestore size={20} />
               </button>
             }
