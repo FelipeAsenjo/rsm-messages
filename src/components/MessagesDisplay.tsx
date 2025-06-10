691c252cccdcb91d244a5cb239fa56f47aabc211
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-06-10 15:55:14 -0400

    Retry button on error messages

--- src/components/MessagesDisplay.tsx
+++ src/components/MessagesDisplay.tsx
@@ -1,3 +1,5 @@
+import { MdOutlineSettingsBackupRestore } from "react-icons/md";
+
 import { useChatStore } from "../store/chatStore";
 import { useUserStore } from "../store/userStore";
 
@@ -7,11 +9,20 @@ export default function MessagesDisplay() {
   const { messages } = useChatStore(state => state)
   const { username } = useUserStore(state => state)
 
+  const handleClick = (id: number | undefined) => {
+    console.log('id', id)
+  }
+
   console.log('messages', messages)
   return (
     <section className="flex-1 flex flex-col-reverse">
         { messages.map(msg => (
           <div key={ msg.id } className='flex flex-col-reverse my-1 overflow-y-auto'>
+            { msg.status === 'error' && 
+              <button className="self-end" onClick={() => handleClick(msg.id)}>
+                <MdOutlineSettingsBackupRestore size={20} />
+              </button>
+            }
             <p 
                 className={msg.sender !== username ? 
                 `${commonMsgStyles} bg-primary-light` : 
