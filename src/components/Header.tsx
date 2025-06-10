3473ab76d8138a4bc5d8a086f7aa2d15eb38f1a8
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-06-10 15:26:17 -0400

    Offline message

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,34 @@
+import { useState, useEffect } from "react"
 import { useStatusStore } from "../store/appStatusStore"
 
 export default function Header() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine)
   const { health } = useStatusStore(state => state)
 
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []); 
+
   return (
-    <header className="flex justify-center bg-primary p-2">
-        <div className="flex flex-row gap-2 w-fit bg-white text-text text-center rounded-full p-2">
-        <div className={`self-center w-4 h-4 rounded-full ${health?.status === 'healthy' ? 'bg-secondary' : 'bg-error'}`}></div>
-        <h1>RSM Chat</h1>
-        </div>
-    </header>
+    <>
+      <header className="flex justify-center bg-primary p-2">
+          <div className="flex flex-row gap-2 w-fit bg-white text-text text-center rounded-full p-2">
+            <div className={`self-center w-4 h-4 rounded-full ${health?.status === 'healthy' ? 'bg-secondary' : 'bg-error'}`}></div>
+            <h1>RSM Chat</h1>
+          </div>
+      </header>
+      { !isOnline && 
+        <div className="bg-error text-center text-white">Offline</div>
+      }
+    </>
   )
 }
