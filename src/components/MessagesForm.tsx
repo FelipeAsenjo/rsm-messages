1ea030c791080b0ea631521cff52ac9e06f04497
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-06-10 02:25:57 -0400

    Handle message form submit

--- src/components/MessagesForm.tsx
+++ src/components/MessagesForm.tsx
@@ -1,8 +1,24 @@
 import React, { useState } from 'react'
 import { IoSend } from "react-icons/io5";
+import type { IPostMessages } from '../interfaces/Chat';
+
+import { useChatStore } from '../store/chatStore';
+import RsmChat from '../services/rsmChatService';
+const rsmChat = new RsmChat()
+
+import IndexedDB from '../services/indexedDB';
+const idxDB = new IndexedDB()
+
+const initialFormValues: IPostMessages = {
+  status: null,
+  sender: 'Felipe', //! REPLACE FOR INDEXED DB USER
+  message: '',
+  timestamp: null
+}
 
 const MessagesForm = React.memo(() => {
-  const [formValues, setFormValues] = useState({})
+  const { addMessage, updateMessageStatus } = useChatStore((state) => state)
+  const [formValues, setFormValues] = useState<IPostMessages>(initialFormValues)
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
@@ -13,15 +29,52 @@ const MessagesForm = React.memo(() => {
     }) 
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    console.log('formvalues',formValues)
+    const messageToStore = {
+      ...formValues,
+      status: 'sent' as IPostMessages['status'],
+      timestamp: new Date().toISOString()
+    }
+
+    addMessage(messageToStore)
+    const messageId = await idxDB.saveMessage(messageToStore)
+
+    const serverValues = { ...formValues }
+    setFormValues(initialFormValues)
+
+    try {
+      const res = await rsmChat.postMessages(serverValues)
+
+      const messageFromServer = {
+        sender: "bot",
+        message: res.response,
+        status: res.status,
+        timestamp: res.timestamp
+      }
+
+      addMessage(messageFromServer)
+      updateMessageStatus(Number(messageId), 'success')
+      await idxDB.saveMessage(messageFromServer)
+    } catch(err) {
+      updateMessageStatus(Number(messageId), 'error')
+      await idxDB.updateMessageStatus(Number(messageId), 'error')
+      //! HANDLE ERROR
+      console.log('error', err)
+    }
   }
 
   return (
     <form className="flex self-end w-full gap-2" onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" className="rounded-lg p-2 shadow-md shadow-primary flex-1" name="message" placeholder="Type a message..." onChange={(e) => handleInputChange(e)} />
+        <input 
+          type="text" 
+          className="rounded-lg p-2 shadow-md shadow-primary flex-1" 
+          name="message" 
+          placeholder="Type a message..." 
+          value={ formValues.message }
+          onChange={(e) => handleInputChange(e)} 
+        />
         <button type="submit" className="bg-primary rounded-full p-2 hover:scale-105 ease-in-out duration-200">
             <IoSend size={28} color="white" />                
         </button>
